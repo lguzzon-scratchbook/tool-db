f14896766e95b5be64f1918ec50d6e6e0cd58782
Luca Guzzon <luca.guzzon@gmail.com>
2025-02-19 13:31:01 +0100

    chore(tests): 🔧 - add network-base test for ToolDb connection retries; update launch configuration in VSCode

---
+++ __tests__/bun/_test_network-base.ts
@@ -0,0 +1,52 @@
+import { ToolDb } from "../../packages/tool-db";
+
+import ToolDbLeveldb from "../../packages/leveldb-store/dist";
+import ToolDbWebsockets from "../../packages/websocket-network/dist";
+import ToolDbWeb3 from "../../packages/web3-user/dist";
+
+import { describe, test, beforeAll,afterAll, expect } from "bun:test";
+
+let nodeA: ToolDb;
+describe("network-base", () => {
+
+  beforeAll(() => {
+  })
+
+  afterAll((done) => {
+    if (nodeA) {
+      (nodeA.network as any).server.close();
+    }
+    setTimeout(done, 500);
+  });
+
+  test("A can retry connection", (done) => {
+    const Alice = new ToolDb({
+      server: false,
+      maxRetries: 1000,
+      peers: [{ host: "localhost", port: 8001 }],
+      storageName: "test-base-client",
+      storageAdapter: ToolDbLeveldb,
+      networkAdapter: ToolDbWebsockets,
+      userAdapter: ToolDbWeb3,
+    });
+    Alice.anonSignIn();
+    Alice.onConnect = () => {
+      expect(Alice.isConnected).toBeTruthy();
+      done();
+    };
+
+    setTimeout(() => {
+      nodeA = new ToolDb({
+        server: true,
+        host: "127.0.0.1",
+        port: 8001,
+        storageName: "test-base-server",
+        storageAdapter: ToolDbLeveldb,
+        networkAdapter: ToolDbWebsockets,
+        userAdapter: ToolDbWeb3,
+      });
+      nodeA.anonSignIn();
+      expect(Alice.isConnected).toBeFalsy();
+    }, 3000);
+  });
+});
